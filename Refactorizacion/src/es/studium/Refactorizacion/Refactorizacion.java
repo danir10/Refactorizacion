083d9fd191d29db63a26bf51ffb721eff928d129
danir10 <depresetion45@gmail.com>
2019-04-01 12:36:42 +0200

    Calcular precio iva

--- Refactorizacion/src/es/studium/Refactorizacion/Refactorizacion.java
+++ Refactorizacion/src/es/studium/Refactorizacion/Refactorizacion.java
@@ -6,6 +6,6 @@ public class Refactorizacion
 	public double calcularPrecio (int unidades, double precioUnitario) {
 		
 		
-		return (unidades*precioUnitario)*(1.21);
+		return (unidades*precioUnitario)*(1.21); 
 	}
 }
