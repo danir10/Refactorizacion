98cfa5e463dbc8247af6cb0c05ec426ed6c6a024
danir10 <depresetion45@gmail.com>
2019-04-01 12:53:59 +0200

    Ejercicio Precio con IVA

--- Refactorizacion/src/es/studium/Refactorizacion/Refactorizacion.java
+++ Refactorizacion/src/es/studium/Refactorizacion/Refactorizacion.java
@@ -4,8 +4,9 @@ public class Refactorizacion
 {
 
 	public double calcularPrecio (int unidades, double precioUnitario) {
-		
-		
+
+		final double iva = 1.21;
 		return (unidades*precioUnitario)*(1.21); 
 	}
+
 }
