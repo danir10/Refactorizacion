ffdafc58e8bd498d47fefae317a1480ea4fc9a16
danir10 <depresetion45@gmail.com>
2019-04-01 13:16:12 +0200

    Ejercicio Precio con constante iva

--- Refactorizacion/src/es/studium/Refactorizacion/Refactorizacion.java
+++ Refactorizacion/src/es/studium/Refactorizacion/Refactorizacion.java
@@ -3,10 +3,12 @@ package es.studium.Refactorizacion;
 public class Refactorizacion
 {
 
+	private static final double Iva = 1.21;
+
 	public double calcularPrecio (int unidades, double precioUnitario) {
 
-		final double iva = 1.21;
-		return (unidades*precioUnitario)*(1.21); 
+		
+		return (unidades*precioUnitario)*Iva; 
 	}
 
 }
