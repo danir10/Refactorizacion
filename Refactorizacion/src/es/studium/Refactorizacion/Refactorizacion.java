a06e478d5cbec1ca48684da632231f37a0fd3c37
danir10 <depresetion45@gmail.com>
2019-04-01 12:34:17 +0200

    Calcular precio con iva

---
+++ Refactorizacion/src/es/studium/Refactorizacion/Refactorizacion.java
@@ -0,0 +1,11 @@
+package es.studium.Refactorizacion;
+
+public class Refactorizacion
+{
+
+	public double calcularPrecio (int unidades, double precioUnitario) {
+		
+		
+		return (unidades*precioUnitario)*(1.21);
+	}
+}
